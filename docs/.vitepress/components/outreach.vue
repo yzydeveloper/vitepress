bb11217167ab0e9ab9c9565c8358823b90c34a5d
yzydeveloper <wqrdw@qq.com>
2021-05-05 23:41:29 +0800

    自定义组件

---
+++ docs/.vitepress/components/outreach.vue
@@ -0,0 +1,16 @@
+<template>
+    <a :href="link">
+        <slot></slot>
+    </a>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+    name: 'Outreach',
+    props: {
+        link: String,
+    },
+    setup(props) {},
+})
+</script>
--- docs/.vitepress/components/show-resource.vue
+++ docs/.vitepress/components/show-resource.vue
@@ -1,9 +1,30 @@
 <template>
-    <div>资源</div>
+    资源{{path}}
 </template>
 
-<script>
-export default {
-    name: "ShowResource"
-}
+<script lang="ts">
+// const modulesFiles = import.meta.globEager('./module/*.js') // vite
+import { defineComponent } from "vue";
+// import {
+//     useRoute,
+//     useSiteData,
+//     usePageData,
+//     useSiteDataByRoute,
+// } from 'vitepress'
+export default defineComponent({
+    name: "ShowResource",
+    props: {
+        path: String,
+    },
+    setup(props) {
+        const modulesFiles = import.meta.glob("/public/design/honeydew2.0/*"); // vite
+        console.log(import.meta);
+        console.log(modulesFiles);
+
+        // console.log(useRoute())
+        // console.log(useSiteData().value)
+        // console.log(usePageData().value)
+        // console.log(useSiteDataByRoute().value)
+    },
+});
 </script>
